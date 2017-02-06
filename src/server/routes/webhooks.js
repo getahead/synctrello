866b04d4677533fad7250cb1ab42e082c2b6d409
Michael Petrov <imrealpunk@gmail.com>
2017-02-07 02:21:38 +0300

    webhook try

--- src/server/routes/webhooks.js
+++ src/server/routes/webhooks.js
@@ -2,9 +2,10 @@ import express from 'express';
 
 const router = express.Router();
 
-router.get('/response/', (req, res, next) => {
+router.get('*', (req, res, next) => {
   console.log(req.query)
   console.log(req.body)
+  console.log(req)
 
   res.send({});
 });
